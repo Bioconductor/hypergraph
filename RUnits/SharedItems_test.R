2cbde30bcf47e02b5c4cbdedde46ae427af3bdba
Seth Falcon <sfalcon@fhcrc.org>
2005-08-10 00:15:18 +0000

    fix unit tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/hypergraph@13523 bc3139a8-67e5-0310-9ffc-ced21a209358

--- RUnits/SharedItems_test.R
+++ RUnits/SharedItems_test.R
@@ -1,4 +1,4 @@
-getP2CMat <- function() {
+simpleCategoryMatrix <- function() {
     ## Create a fictional protein <--> complex matrix by hand
     c1 <- c(1, 1, 0, 0, 0, 1, 1)
     c2 <- c(0, 1, 0, 0, 0, 1, 1)
@@ -8,22 +8,27 @@ getP2CMat <- function() {
     p2c <- cbind(c1, c2, c3, c4)
     rownames(p2c) <- paste("p", 1:nrow(p2c), sep="")
     colnames(p2c) <- paste("C", 1:ncol(p2c), sep="")
-    p2c
+    CategoryMatrix(p2c)
 }
+    
 
 testFindSharedProteinStructuresEmpty <- function() {
-    p2c <- getP2CMat()
-    shared <- findSharedItemSets(p2c, 5)
-    checkEquals(character(0), ls(shared))
+    p2c <- simpleCategoryMatrix()
+    result <- new.env()
+    result <- findSharedItemSets(p2c, 5, result)
+    checkEquals(character(0), ls(result$itemSetCache))
 }
 
 
 testFindSharedProteinStructures <- function() {
-    p2c <- getP2CMat()
-    shared <- findSharedItemSets(p2c, 3)
+    p2c <- simpleCategoryMatrix()
+    shared <- new.env()
+    shared <- findSharedItemSets(p2c, 3, shared)
+    shared <- shared$itemSetCache
     checkEquals(1, length(ls(shared)))
 
-    shared <- findSharedItemSets(p2c, 2)                
+    shared <- findSharedItemSets(p2c, 2)
+    shared <- shared$itemSetCache
     checkEquals(c(1, 2), categories(shared[["2-6-7"]]))
     checkEquals(c(2, 6, 7), items(shared[["2-6-7"]]))
 
